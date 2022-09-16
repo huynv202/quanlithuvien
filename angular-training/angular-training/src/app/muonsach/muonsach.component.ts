d5262d3d02c64ff015e9163064c8af4764df457b
Nguyenvanhuy111-tech <77135650+Nguyenvanhuy111-tech@users.noreply.github.com>
2022-09-16 18:05:13 +0700

    Ok

---
+++ angular-training/angular-training/src/app/muonsach/muonsach.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-muonsach',
+  templateUrl: './muonsach.component.html',
+  styleUrls: ['./muonsach.component.scss']
+})
+export class MuonsachComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ angular-training/angular-training/src/app/nxb/nxb.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nxb',
+  templateUrl: './nxb.component.html',
+  styleUrls: ['./nxb.component.scss']
+})
+export class NxbComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
